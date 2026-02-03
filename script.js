6385851a5090c568b8d44dff35c612da9c2074e2
Sgrodotv <yaghmada@yahoo.de>
2026-02-03 22:19:13 +0100

    Update script.js

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 const SUPABASE_URL = "https://mvladicfytndyekrbzme.supabase.co";
 const SUPABASE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im12bGFkaWNmeXRuZHlla3Jiem1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAxMzM4MjksImV4cCI6MjA4NTcwOTgyOX0.eeTXlaU-DYqQqV5h-FaRsTigRvGKLhLVBvNHvCE2DJ4";
 
-const supabaseClient = window.supabase.createClient(
+const db = window.supabase.createClient(
   SUPABASE_URL,
   SUPABASE_KEY
 );
@@ -94,4 +94,5 @@ async function loadEntries() {
     entriesDiv.appendChild(div);
   });
 }
+
 
